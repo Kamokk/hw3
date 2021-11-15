8c49d9cdda19828780b477ada40e688c3df82275
C1 <yarievakamila@gmail.com>
2021-11-15 17:49:06 +0300

    add remane var

--- src/main/java/ru/yarieva/Main.java
+++ src/main/java/ru/yarieva/Main.java
@@ -4,35 +4,35 @@ public class Main {
     public static void main(String[] args) {
         //Создаем родительский объект насекомое
         Bug bug = new Bug("Насекомое", 5, "Зеленый");
-        String nameBug = bug.getName();
-        int sizeBug = bug.getSize();
-        String colorBug = bug.getColor();
+        String bugName = bug.getName();
+        int bugSize = bug.getSize();
+        String bugColor = bug.getColor();
         System.out.println("Создаем родительский объект насекомое");
-        System.out.println("наименование- " + nameBug + " Размер " + sizeBug + " Окрас " + colorBug);
+        System.out.println("наименование- " + bugName + " Размер " + bugSize + " Окрас " + bugColor);
         System.out.println("Умеет делать");
         Bug.sitDown();
         System.out.println();
 
         //Создаем дочерний объект бабочка
         Butterfly butterfly = new Butterfly("Бабочка", 10, "Оранжевый");
-        String nameButterfly = butterfly.getName();
-        int sizeButterfly = butterfly.getSize();
-        String colorButterfly = butterfly.getColor();
+        String butterflyName = butterfly.getName();
+        int butterflySize = butterfly.getSize();
+        String butterflyColor = butterfly.getColor();
         System.out.println("Создаем дочерний объект бабочка");
-        System.out.println("наименование- " + nameButterfly + " Размер " + sizeButterfly + " Окрас " + colorButterfly);
+        System.out.println("наименование- " + butterflyName + " Размер " + butterflySize + " Окрас " + butterflyColor);
         System.out.println("Умеет делать");
         Butterfly.sitDown();
         Butterfly.fly();
         Butterfly.collectPollen();
         System.out.println();
 
-        //Создаем дочерний объект Кузнечик
+        //Создаем дочерний объект кузнечик
         Grig grig = new Grig("Кузнечик", 4, "Салатовый");
-        String nameGrig = grig.getName();
-        int sizeGrig = grig.getSize();
-        String colorGrig = grig.getColor();
+        String grigName = grig.getName();
+        int grigSize = grig.getSize();
+        String grigColor = grig.getColor();
         System.out.println("Создаем дочерний объект кузнечик");
-        System.out.println("наименование- " + nameGrig + " Размер " + sizeGrig + " Окрас " + colorGrig);
+        System.out.println("наименование- " + grigName + " Размер " + grigSize + " Окрас " + grigColor);
         System.out.println("Умеет делать");
         Grig.sitDown();
         Grig.jump();
